eeb98787760fda3d83047d415508322dca5ce329
CristianQuijano750 <91923330+CristianQuijano750@users.noreply.github.com>
2021-11-02 19:26:50 -0600

    AdminLTE 1 al 3 de los videos listo
    se realizo lo visto en los videos de la plataforma de Y0utub3

---
+++ resources/views/mascotas.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@section('titulo','CRUD MASCOTAS')
+@section('contenido')
+	<h1>HOLA BIENVENIDO</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,8 @@ Route::get('Desencriptar', function(){
 
 Route::post('validar','AccesoController@validar');
 
+
+Route::get('mascotas', function () {
+    return view('mascotas');
+});
+
