91646341b222ffab0dbb48e2a7bf4750d2f3a3cc
CristianQuijano750 <tecnico18245@gmail.com>
2021-12-01 18:31:19 -0600

    completo crud ventas
    ya esta nota correg1r la l1sta porque sale ][

---
+++ public/js/apis/apiVenta.js
@@ -0,0 +1,114 @@
+function init(){
+var apiProducto='http://localhost/mascotas/public/apiProducto';
+new Vue({
+	//asignamos el token
+	http: {
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('#token').getAttribute('value')
+            }
+        },
+
+	el:'#apiVenta',
+
+	data:{
+		mensaje:'HOLA MUNDO',
+		sku:'',
+		ventas:[],
+		cantidades:[],
+		cant:1,
+		auxSubTotal:0,
+		
+
+	},
+
+	//sejecuta automaticamente cuando la pagina se crea
+	created:function(){
+		
+
+	},
+
+	//inicio de methods
+	methods:{
+		buscarProducto:function(){
+
+			if(this.sku) {
+			var producto = {}
+			this.$http.get(apiProducto + '/' + this.sku).then(function(j){
+				producto = {
+					sku:j.data.sku,
+					nombre:j.data.nombre,
+					precio:j.data.precio,
+					cantidad:1,
+					total:j.data.precio
+				};
+			
+					this.ventas.push(producto);
+					this.cantidades.push(1);
+				this.sku='';
+			});
+		}
+
+		},
+
+		eliminarProducto:function(id){
+			this.ventas.splice(id,1);
+		}
+		
+
+	},
+	//fin de methods
+
+	computed:{
+		totalProducto(){  
+			return(id)=>{
+				var total = 0;
+				total=this.ventas[id].precio * this.cantidades[id];
+				//actualizo el total de ventas del producto en el array ventas
+				this.ventas[id].total=total;
+				//actualizo la cantidad en el array ventas
+				this.ventas[id].cantidad=this.cantidades[id];
+				return total.toFixed(1);
+			}
+
+		},
+		//finde total producto
+
+		subTotal(){
+			var total=0;
+			var valor=0;
+
+			for (var i =  this.ventas.length - 1; i >= 0; i--) {
+				 total=total+this.ventas[i].total;
+			}
+			//mando una copia del subtotal a la seccion del data para el uso de otros calculos (auxsubtotal)
+			this.auxSubTotal=total.toFixed(1);
+			return total.toFixed(1);
+		},
+
+		iva(){
+			var auxIva=0;
+			auxIva=this.auxSubTotal*0.16;
+
+			return auxIva.toFixed(1);
+		},
+
+		granTotal(){
+			var auxTotal=0;
+			auxTotal=this.auxSubTotal*1.16;
+			return auxTotal.toFixed(1);
+		},
+
+		noArticulos(){
+			var acum=0;
+			for (var i = this.ventas.length - 1; i >= 0; i--) {
+				acum=acum+this.ventas[i].cantidad;
+			}
+			return acum;
+		}
+
+
+	}
+	// fin de computed
+})
+
+} window.onload = init;
--- resources/views/ventas.blade.php
+++ resources/views/ventas.blade.php
@@ -1,24 +1,112 @@
 @extends('layouts.master')
-@section('titulo','VENTAS')
+@section('titulo','Interface de ventas')
 @section('contenido')
 	
-<div id="apiVenta">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
+<div id="apiVenta"> 
+		<div class="container"> <!-- inicio de container -->
+			<div class="row"> <!-- inicio de row -->
+				<div class="col-md-4"> <!-- inicio de col -->
 
 					<div class="input-group mb-3">
-  						<input type="text" class="form-control" placeholder="Escriba el codigo del producto" aria-label="Recipient's username" aria-describedby="basic-addon2">
+  						<input type="text" class="form-control" placeholder="Escriba el codigo del producto" aria-describedby="basic-addon2" v-model="sku" v-on:keyup.enter="buscarProducto()">
   					<div class="input-group-append">
-  						<button class="btn btn-outline-secondary" type="button">Button</button>
+  						<button class="btn btn-outline-secondary" type="button" @click="buscarProducto()">Búscar</button>
   					</div>
 				</div>
 
-			</div>
+			</div> <!-- fin del col -->
 			
+		</div> <!-- fin de row -->
+	
+
+		<div class="card">
+		<!-- inicio de row -->
+			<div class="card-body">
+				<div class="row">
+					<div class="col-md-12">
+
+						<table class="table table-bordered">
+							<thead>
+								<th style="background: #ffff66">SKU</th>
+								<th style="background: #ffff66">PRODUCTO</th>
+								<th style="background: #ffff66">OPERACIONES</th>
+								<th style="background: #ffff66">PRECIO</th>
+								<th style="background: #ffff66">CANTIDAD</th>
+								<th style="background: #ffff66">TOTAL</th>
+							</thead>
+
+							<tbody>
+								<tr v-for="(venta,index) in ventas">
+									<td>@{{venta.sku}}</td>
+									<td>@{{venta.nombre}}</td>
+									<td>
+										<button class="btn btn-default btn-xs" @click="eliminarProducto(index)">
+											<i class="fas fa-trash"></i>
+										</button>
+									</td>
+									<td>@{{venta.precio}}</td>
+									<td><input type="number" v-model.number="cantidades[index]" min="1"></td>
+									<td>@{{totalProducto(index)}}</td>
+								</tr>
+							</tbody>
+
+						</table>
+						
+					</div>
+				</div>
+				<!-- fin del row -->
+			</div>
+			<!-- fin del card body -->
 		</div>
-	</div>
-</div>	
+		<!-- fin de card -->
+		@{{ventas}}
+
+		<div class="row">
+			<div class="col-md-8"></div>
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-body">
+							
+								<table class="table table-bordered table-condensed">
+									<tr>
+										<th style="background: #ffff66">Subtotal</th>
+										<td>$ @{{subTotal}}</td>
+
+									</tr>
+									
+									<tr>
+										<th style="background: #ffff66">IVA</th>
+										<td>$ @{{iva}}</td>
+
+									</tr>
+									<tr>
+										<th style="background: #ffff66">TOTAL</th>
+										<td><b>$ @{{granTotal}}</b></td>
+									</tr>
+									<tr>
+										<th style="background: #ffff66">Articulos: </th>
+										<th>@{{noArticulos}}</th>
+
+									</tr>
+
+								</table>
+								
+						</div>
+						<!-- fin de card-body -->
+
+					</div>
+					  <!-- fin de card -->
+				</div>
+					<!-- fin de col md 4 -->
+
+		</div>
+		<!-- fin de row -->
+
+
+
+
+	</div> <!-- fin de container -->
+</div> <!-- fin de vue -->
 	
 @endsection
 
@@ -26,7 +114,8 @@
 @push('scripts')
 
 	<script type="text/javascript" src="{{asset('js/vue-resource.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/apis/apiMascota.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/apis/apiVenta.js')}}"></script>
 
 @endpush
-<input type="hidden" name="route" value="{{url('/')}}">
+
+<!-- <input type="hidden" name="route" value="{{url('/')}}"> -->
